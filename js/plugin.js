6cf02d448b68856fa9f9dbba03c4d19587ec85f8
hungnm11 <hung.nguyen@hotmail.com.vn>
2015-04-25 19:02:33 +0700

    update codes

--- js/plugin.js
+++ js/plugin.js
@@ -3,7 +3,17 @@
     this.Modal = function() {
         
         var defaults = {
-            mpdURL: ""
+            mpdURL: "",
+            screenSize: {
+                maxWidthScreen: 640,
+                maxHeightScreen: 480
+            },
+            advertisement: {
+                setTimeStart: 2,
+                setAdvertisementTime: 10,
+                setContent: ""
+            },
+            poster: ""
         }
         
         if (arguments[0] && typeof(arguments[0]) === "object") {
@@ -21,13 +31,25 @@
         return source;
     }
     
+    Modal.prototype.setScreenSize = function() {
+        this.vid = document.querySelector('video');
+        this.vid.setAttribute('width', this.options.screenSize.maxWidthScreen);
+        this.vid.setAttribute('height', this.options.screenSize.maxHeightScreen);
+        this.vid.setAttribute('poster', this.options.poster);
+    };
+    
     Modal.prototype.streamDASH = function(elem) {
-        var url = this.options.mpdURL; 
+        
+        var url = this.options.mpdURL;
         var context = new Dash.di.DashContext();
         var player = new MediaPlayer(context);
         player.startup();
         player.attachView(elem);
         player.attachSource(url);
-    }
+                
+        this.setScreenSize();
+
+    };
+    
     
 })();
